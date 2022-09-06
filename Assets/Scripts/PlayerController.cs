c8d326d6767b191da1a7ff6a8c70a02d9b46f663
Rushil Desai <66389126+rush-bot@users.noreply.github.com>
2022-09-06 01:05:10 -0700

    Assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+
+    private float movementX;
+    private float movementY;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>(); 
+		count = 0;
+		SetCountText ();
+        winTextObject.SetActive(false);
+	}
+
+	void FixedUpdate ()
+	{
+		Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+        void OnMove(InputValue value)
+        {
+        	Vector2 v = value.Get<Vector2>();
+        	movementX = v.x;
+        	movementY = v.y;
+        }
+
+        void SetCountText()
+	{
+		countText.text = "XR Rocks - Rushil Desai: " + count.ToString();
+		if (count >= 12) 
+		{
+            winTextObject.SetActive(true);
+		}
+	}
+}
